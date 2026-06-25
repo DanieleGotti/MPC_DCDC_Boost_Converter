2b46b254a4e45a75121243d77bef08b8a0d4645d
DanieleGotti <danielegotti02@gmail.com>
2026-06-25 12:09:02 +0200

    Add mpc

--- code/main.m
+++ code/main.m
@@ -2,15 +2,13 @@ clear;
 clc;
 close all
 
-addpath(genpath('/MPC_DCDC_Boost_Converter/code/tbxmanager'));
-
 set(0,'DefaultLineLineWidth', 1.5);
 set(0,'defaultAxesFontSize', 14);
 set(0,'DefaultFigureWindowStyle', 'docked'); 
 set(0,'defaulttextInterpreter','latex');
 rng('default');
 
-%%  0. Parametri del DC-DC Boost Converter (in SI units)
+%%  0. Parametri del DC-DC Boost Converter 
 T = 0.65e-3;    % [s]
 L = 4.2e-3;     % [H]
 C = 2200e-6;    % [F]
@@ -19,9 +17,9 @@ Vin = 15;       % [V]
 
 %%  1. Invariant set per il sistema controllato
 
-% Riferimento fisico del sistema (Equilibrio)
+% Riferimento fisico del sistema (equilibrio)
 x_ref = [0.389; -16];
-u_ref = 0.516e-3; 
+u_ref = 0.516e-3; % MILLISECONDI O PERCENTUALI???
 
 % Sistema linearizzato al discreto valutato in (x_ref, u_ref)
 A = [1,                         (T/L)*(1 - u_ref);
@@ -33,9 +31,9 @@ B = [-(T/L)*(x_ref(2) - Vin);
 % Vincoli fisici assoluti
 iL_min = -1.6;      iL_max = 2.4;       
 vo_min = -19;       vo_max = -13;       
-dc_min = 0.3e-3;    dc_max = 0.75e-3;   
+dc_min = 0.3e-3;    dc_max = 0.75e-3;  % MILLISECONDI O PERCENTUALI???
 
-% Vincoli su stato e ingresso (NELLE COORDINATE DEL SISTEMA LINEARIZZATO!)
+% Vincoli su stato e ingresso (nelle coordinate del sistema linearizzato)
 % Trasliamo i vincoli sottrando l'equilibrio
 Hx = [eye(2); -eye(2)];
 hx = [iL_max - x_ref(1); 
@@ -80,3 +78,70 @@ hold on;
 CIS.plot('Color', 'r', 'Alpha', 0.8);
 grid on;
 
+%%  3. Design MPC e simulazione
+
+% Numero di step simulati
+T_sim = 60;
+
+% Riferimento del sistema linearizzato
+x_ref_lin = [0; 0];
+u_ref_lin = 0;
+
+% Stato iniziale
+x_0 = [2.3, -12.5];
+
+mpc = mpc_ingredients(A,B,Hx,hx,Hu,hu,CIS_H,CIS_h,x_ref_lin,u_ref_lin,Q,R,N);
+
+% Log stati e ingresso sistema
+x_log = zeros(2,T_sim+1);
+u_log = zeros(1,T_sim);
+flags = zeros(1,T_sim);
+
+x_log(:,1) = x_0;
+
+for tt = 1:T_sim
+
+    % Stato sistema linearizzato
+    x_lin = x_log(:,tt) - x_ref;
+    x_lin_shifted = x_lin - x_ref_lin;
+
+    % Impostazioni MPC relative alla condizione iniziale
+    f = mpc.f_base * x_lin_shifted;
+    b_ineq = mpc.b_ineq_base - mpc.b_ineq_x0_factor * x_lin_shifted;
+    
+    % Risoluzione problema di ottimizzazione
+    [delta_u_seq,~,exitflag] = quadprog(mpc.F,f,mpc.A_ineq,b_ineq);
+
+    flags(tt) = exitflag;
+
+    if exitflag ~= 1
+        warning('Quadprog fallito allo step %d. (Exitflag: %d).', tt, exitflag);
+    end
+
+    % Azione di controllo fisica
+    u_log(tt) = u_ref + delta_u_seq(1);
+    
+    % Risposta del sistema
+    x_log(:,tt+1) = boost_converter(x_log(:,tt), u_log(tt), T, L, C, R, Vin);
+
+end
+
+%%  4. Plot dei risultati
+
+% Traslazione del CIS e dell'N-step set nelle coordinate originali
+CIS_shifted = CIS + x_ref;
+Np_step_set_shifted = Np_step_set + x_ref;
+
+figure(3);
+Np_step_set_shifted.plot('Alpha',0);
+title('\textbf{Traiettoria del sistema}');
+xlabel('$i_L$ [A]');
+ylabel('$v_o$ [V]');
+hold on;
+CIS_shifted.plot('Alpha', 0.1)
+hold on
+plot(x_log(1,:),x_log(2,:),'Color',[0 0 0.5])
+scatter(x_log(1,:),x_log(2,:),'cyan');
+xlim([iL_min-0.2, iL_max+0.2]);
+ylim([vo_min-0.5, vo_max+0.5]);
+grid on;
